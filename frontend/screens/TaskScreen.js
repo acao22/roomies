efa8a52f7b6712e6429576163a51f55202ba923d
Angie Cao <caoangelina@gmail.com>
2025-03-11 23:23:50 -0700

    updated tasks page to have animation, reformat due dates, added avatars. removed bypass from landing page

--- frontend/screens/TaskScreen.js
+++ frontend/screens/TaskScreen.js
@@ -1,72 +1,57 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  FlatList,
-  Animated,
-  Easing,
-} from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, TouchableOpacity, FlatList, Image } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import CalendarScreen from "./CalendarScreen";
 import {
   useSharedValue,
   useAnimatedStyle,
   withTiming,
+  Easing,
 } from "react-native-reanimated";
+import Animated from "react-native-reanimated";
+import * as Animatable from "react-native-animatable";
 
-// helper function for formatting due dates prettily
-const formatDueDate = (dateStr) => {
-  const today = new Date();
-  const tomorrow = new Date(today);
-  tomorrow.setDate(today.getDate() + 1);
+// HARDCODED SHART FOR NOW
 
-  const dueDate = new Date(dateStr);
-  const options = { weekday: "long", month: "short", day: "numeric" };
-
-  if (
-    dueDate.getFullYear() === tomorrow.getFullYear() &&
-    dueDate.getMonth() === tomorrow.getMonth() &&
-    dueDate.getDate() === tomorrow.getDate()
-  ) {
-    return "Due tomorrow";
-  }
-
-  return `Due ${dueDate.toLocaleDateString("en-US", options)}`;
+const userAvatars = {
+  Luna: require("../images/avatar1.png"),
+  Andrew: require("../images/avatar2.png"),
+  Angie: require("../images/avatar3.png"),
+  Default: require("../images/avatar4.png"),
 };
 
 const initialTasks = [
   {
     id: "1",
-    title: "Vacuum",
+    title: "vacuum",
     assignedTo: ["Luna"],
-    status: "open",
-    dueDate: "2025-03-14",
-  },
-  {
-    id: "2",
-    title: "Dishes",
-    assignedTo: ["Andrew", "Luna"],
     status: "open",
     dueDate: "2025-03-12",
   },
   {
+    id: "2",
+    title: "dishes",
+    assignedTo: ["Andrew", "Luna", "Angie"],
+    status: "open",
+    dueDate: "2025-03-15",
+  },
+  {
     id: "3",
-    title: "Clean",
+    title: "clean",
     assignedTo: ["Everyone"],
     status: "open",
     dueDate: "2025-03-12",
   },
   {
     id: "4",
-    title: "Hello",
+    title: "hello",
     assignedTo: ["Andrew"],
     status: "completed",
     dueDate: "2025-03-10",
   },
   {
     id: "5",
-    title: "Bye",
+    title: "bye",
     assignedTo: ["Andrew"],
     status: "completed",
     dueDate: "2025-03-09",
@@ -80,10 +65,47 @@ const initialTasks = [
   },
 ];
 
+// helper function for formatting due dates
+const formatDueDate = (dateStr) => {
+  const today = new Date();
+  // normalizing time to compare only date
+  today.setHours(0, 0, 0, 0);
+
+  const tomorrow = new Date(today);
+  tomorrow.setDate(today.getDate() + 1);
+
+  // convert dueDate string -> Date object
+  const [year, month, day] = dateStr.split("-").map(Number);
+  const dueDate = new Date(year, month - 1, day);
+
+  const options = { weekday: "short", month: "short", day: "numeric" };
+
+  if (dueDate < today) {
+    // past due: red color + alert icon
+    return (
+      <Text className="text-red-700 flex-row items-center">
+        <Ionicons name="alert-circle-sharp" size={14} className="mr-1" />
+        {` past due - ${dueDate.toLocaleDateString("en-US", options)}`}
+      </Text>
+    );
+  }
+
+  if (
+    dueDate.getFullYear() === tomorrow.getFullYear() &&
+    dueDate.getMonth() === tomorrow.getMonth() &&
+    dueDate.getDate() === tomorrow.getDate()
+  ) {
+    // due tmr: some important color maybe
+    return <Text>due tomorrow</Text>;
+  }
+  return <Text>{dueDate.toLocaleDateString("en-US", options)}</Text>;
+};
+
 export default function TaskScreen() {
   const [tasks, setTasks] = useState(initialTasks);
   const [activeTab, setActiveTab] = useState("tasks");
 
+  // helper function to toggle task completion status
   const toggleTaskCompletion = (taskId) => {
     setTasks((prevTasks) =>
       prevTasks.map((task) =>
@@ -97,37 +119,99 @@ export default function TaskScreen() {
     );
   };
 
+  // helper function to sort upcoming tasks by due date
+  const upcomingTasks = tasks
+    .filter((t) => t.status === "open")
+    .sort((a, b) => {
+      // convert dueDate to Date object
+      // idk i did this before to but soemtimes it tweaks out... can clean later
+      const [yearA, monthA, dayA] = a.dueDate.split("-").map(Number);
+      const [yearB, monthB, dayB] = b.dueDate.split("-").map(Number);
+
+      const dateA = new Date(yearA, monthA - 1, dayA);
+      const dateB = new Date(yearB, monthB - 1, dayB);
+
+      // sorting in ASCNEDING order
+      return dateA - dateB;
+    });
+
+  // completed tasks should be separate
+  const completedTasks = tasks.filter((t) => t.status === "completed");
+
   const renderTaskItem = ({ item, index }) => {
     const isCompleted = item.status === "completed";
+
+    // rendering alternating colors of tasks
     const backgroundColor = isCompleted
-      ? "bg-custom-pink-200"
+      ? "bg-custom-pink-100"
       : index % 2 === 0
       ? "bg-custom-yellow"
       : "bg-custom-blue-100";
     const textColor = isCompleted
       ? "text-custom-tan line-through"
-      : "text-white";
+      : index % 2 === 0
+      ? "text-custom-blue-200"
+      : "text-custom-tan";
+    const iconColor = isCompleted
+      ? "#FEF9E5"
+      : index % 2 === 0
+      ? "#788ABF"
+      : "#FEF9E5";
 
     return (
-      <View
-        className={`flex-row items-center ${backgroundColor} rounded-xl p-4 mb-3`}
+      <Animatable.View
+        animation={isCompleted ? "lightSpeedIn" : "slideInUp"}
+        duration={isCompleted ? 500 : 350}
+        delay={index * 100} // stagger animation a little for each item
+        // easing={"cubic"}
+        className={`flex-row items-center ${backgroundColor} rounded-xl p-4 mb-3 shadow-sm`}
       >
+        {/* CHECKBOX */}
         <TouchableOpacity onPress={() => toggleTaskCompletion(item.id)}>
           <Ionicons
-            name={isCompleted ? "checkmark-circle" : "square-outline"}
+            name={isCompleted ? "checkbox" : "square-outline"}
             size={32}
-            color={"#FEF9E5"}
+            color={`${iconColor}`}
             className="mr-3"
           />
         </TouchableOpacity>
+
+        {/* TASK DETAILS */}
         <View className="flex-1">
-          <Text className={`text-lg font-semibold ${textColor}`}>
+          <Text className={`text-2xl font-spaceGrotesk font-bold ${textColor}`}>
             {item.title}
           </Text>
-          <Text className="text-custom-tan">{formatDueDate(item.dueDate)}</Text>
-          <Text className="text-custom-tan">{item.assignedTo.join(" & ")}</Text>
+          <Text className={`text-s font-spaceGrotesk ${textColor}`}>
+            {formatDueDate(item.dueDate)}
+          </Text>
+          <Text className={`text-s font-spaceGrotesk ${textColor}`}>
+            {item.assignedTo.join(" & ")}
+          </Text>
         </View>
-      </View>
+
+        {/* AVATAR(S) */}
+        <View className="flex-row justify-between z-10">
+          {item.assignedTo.map((user, i) => (
+            <View
+              key={i}
+              style={{
+                backgroundColor: "rgba(254, 249, 229, 0.52)", // opacity lowered
+                borderRadius: 9999, // full rounded just so it works lmao
+                overflow: "hidden",
+                height: 64,
+                width: 64,
+                marginLeft: i === 0 ? 0 : -36,
+              }}
+            >
+              <Image
+                source={userAvatars[user] || userAvatars["Default"]}
+                className="h-full w-full"
+                resizeMode="cover"
+              />
+            </View>
+          ))}
+        </View>
+      </Animatable.View>
     );
   };
 
@@ -140,7 +224,9 @@ export default function TaskScreen() {
           onPress={() => setActiveTab("tasks")}
           className="px-5 py-2 rounded-t-[20px] z-10 bg-custom-pink-200"
         >
-          <Text className="text-2xl font-bold mb-2 text-white">tasks</Text>
+          <Text className="text-2xl font-bold mb-2 text-white font-spaceGrotesk ">
+            tasks
+          </Text>
         </TouchableOpacity>
 
         {/* calendar tab */}
@@ -148,11 +234,13 @@ export default function TaskScreen() {
           onPress={() => setActiveTab("calendar")}
           className="px-5 py-2 rounded-t-[20px] z-10 bg-custom-yellow"
         >
-          <Text className="text-2xl font-bold mb-2 text-white">calendar</Text>
+          <Text className="text-2xl font-bold mb-2 text-white font-spaceGrotesk ">
+            calendar
+          </Text>
         </TouchableOpacity>
       </View>
 
-      {/* CORal border */}
+      {/* CORAL border */}
       <Animated.View className="flex-1 items-center -mt-4 z-10">
         <View
           className={`w-[92%] flex-1 border-[20px] ${
@@ -166,14 +254,15 @@ export default function TaskScreen() {
               <FlatList
                 data={[
                   "upcoming",
-                  ...tasks.filter((t) => t.status === "open"),
+                  ...upcomingTasks,
                   "completed",
-                  ...tasks.filter((t) => t.status === "completed"),
+                  ...completedTasks,
                 ]}
+                showsVerticalScrollIndicator={false}
                 renderItem={({ item, index }) =>
                   item === "upcoming" ? (
-                    <View className="flex-row justify-between items-center my-4 mx-1">
-                      <Text className="text-2xl font-bold text-custom-blue-200">
+                    <View className="flex-row justify-between items-center my-4 mx-1 shadow-md">
+                      <Text className="text-3xl font-bold font-spaceGrotesk text-custom-blue-200">
                         upcoming
                       </Text>
                       <TouchableOpacity>
@@ -185,7 +274,7 @@ export default function TaskScreen() {
                       </TouchableOpacity>
                     </View>
                   ) : item === "completed" ? (
-                    <Text className="text-2xl font-bold text-custom-blue-200 my-4 mx-1">
+                    <Text className="text-3xl font-spaceGrotesk font-bold text-custom-blue-200 my-4 mx-1">
                       completed
                     </Text>
                   ) : (
