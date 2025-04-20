7e9099f51cd3fb0a2f1edb4789d64d83e4934609
Natalie Lim <natalielim1906@gmail.com>
2025-04-20 01:31:05 -0400

    fixed everything but the stupid save function for the avatar. I know it's saves, but the navigation is still broken. I have no clue how to fix i fear:

--- frontend/screens/AvatarCreation.js
+++ frontend/screens/AvatarCreation.js
@@ -15,10 +15,81 @@ import * as FileSystem from "expo-file-system";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 
+const dark_hair = [
+  { id: "1", image: require("../../frontend/assets/dark_hair/hair-f1-dark-new.png") },
+  { id: "2", image: require("../../frontend/assets/dark_hair/hair-f2-dark-new.png") },
+  { id: "3", image: require("../../frontend/assets/dark_hair/hair-f3-dark-new.png") },
+  { id: "4", image: require("../../frontend/assets/dark_hair/hair-f4-dark-new.png") },
+  { id: "5", image: require("../../frontend/assets/dark_hair/hair-f5-dark-new.png") },
+  { id: "6", image: require("../../frontend/assets/dark_hair/hair-f6-dark-new.png") },
+  { id: "7", image: require("../../frontend/assets/dark_hair/hair-f7-dark-new.png") },
+  { id: "8", image: require("../../frontend/assets/dark_hair/hair-f8-dark-new.png") },
+  { id: "9", image: require("../../frontend/assets/dark_hair/hair-f9-dark-new.png") },
+  { id: "10", image: require("../../frontend/assets/dark_hair/hair-m1-dark-new.png") },
+  { id: "11", image: require("../../frontend/assets/dark_hair/hair-m2-dark-new.png") },
+  { id: "12", image: require("../../frontend/assets/dark_hair/hair-m3-dark-new.png") },
+  { id: "13", image: require("../../frontend/assets/dark_hair/hair-m4-dark-new.png") },
+  { id: "14", image: require("../../frontend/assets/dark_hair/hair-m5-dark-new.png") },
+];
+
+const light_hair = [
+  { id: "1", image: require("../../frontend/assets/light_hair/hair-f1-light-new.png") },
+  { id: "2", image: require("../../frontend/assets/light_hair/hair-f2-light-new.png") },
+  { id: "3", image: require("../../frontend/assets/light_hair/hair-f3-light-new.png") },
+  { id: "4", image: require("../../frontend/assets/light_hair/hair-f4-light-new.png") },
+  { id: "5", image: require("../../frontend/assets/light_hair/hair-f5-light-new.png") },
+  { id: "6", image: require("../../frontend/assets/light_hair/hair-f6-light-new.png") },
+  { id: "7", image: require("../../frontend/assets/light_hair/hair-f7-light-new.png") },
+  { id: "8", image: require("../../frontend/assets/light_hair/hair-f8-light-new.png") },
+  { id: "9", image: require("../../frontend/assets/light_hair/hair-f9-light-new.png") },
+  { id: "10", image: require("../../frontend/assets/light_hair/hair-m1-light-new.png") },
+  { id: "11", image: require("../../frontend/assets/light_hair/hair-m2-light-new.png") },
+  { id: "12", image: require("../../frontend/assets/light_hair/hair-m3-light-new.png") },
+  { id: "13", image: require("../../frontend/assets/light_hair/hair-m4-light-new.png") },
+  { id: "14", image: require("../../frontend/assets/light_hair/hair-m5-light-new.png") },
+];
+
+const faces = [
+  { id: "1", image: require("../../frontend/assets/faces/face-1.png") },
+  { id: "2", image: require("../../frontend/assets/faces/face-2.png") },
+  { id: "3", image: require("../../frontend/assets/faces/face-3.png") },
+  { id: "4", image: require("../../frontend/assets/faces/face-4.png") },
+  { id: "5", image: require("../../frontend/assets/faces/face-5.png") },
+  { id: "6", image: require("../../frontend/assets/faces/face-6.png") },
+  { id: "7", image: require("../../frontend/assets/faces/face-7.png") },
+  { id: "8", image: require("../../frontend/assets/faces/face-8.png") },
+  { id: "9", image: require("../../frontend/assets/faces/face-9.png") },
+  { id: "10", image: require("../../frontend/assets/faces/face-10.png") },
+  { id: "11", image: require("../../frontend/assets/faces/face-11.png") },
+  { id: "12", image: require("../../frontend/assets/faces/face-12.png") },
+  { id: "13", image: require("../../frontend/assets/faces/face-13.png") },
+  { id: "14", image: require("../../frontend/assets/faces/face-14.png") },
+  { id: "15", image: require("../../frontend/assets/faces/face-15.png") },
+];
+
+const skin_color = [
+  { id: "1", image: require("../../frontend/assets/skin_colors/skin-0.png") },
+  { id: "2", image: require("../../frontend/assets/skin_colors/skin-1.png") },
+  { id: "3", image: require("../../frontend/assets/skin_colors/skin-2.png") },
+  { id: "4", image: require("../../frontend/assets/skin_colors/skin-3.png") },
+]
+
+const accessories = [
+  { id: "1", image: require("../../frontend/assets/accessories/accessory-1.png") },
+  { id: "2", image: require("../../frontend/assets/accessories/accessory-2.png") },
+  { id: "3", image: require("../../frontend/assets/accessories/accessory-3.png") },
+  { id: "4", image: require("../../frontend/assets/accessories/accessory-4.png") },
+  { id: "5", image: require("../../frontend/assets/accessories/accessory-5.png") },
+  { id: "6", image: require("../../frontend/assets/accessories/accessory-6.png") },
+  { id: "7", image: require("../../frontend/assets/accessories/accessory-7.png") },
+  { id: "8", image: require("../../frontend/assets/accessories/accessory-8.png") },
+  { id: "9", image: require("../../frontend/assets/accessories/accessory-9.png") },
+]
+
 const AvatarCreation = ({ }) => {
   const [isToggled, setIsToggled] = useState(false);
-  const [selectedHair, setSelectedHair] = useState(null);
-  const [selectedFace, setSelectedFace] = useState(null);
+  const [selectedHair, setSelectedHair] = useState(dark_hair[0]); // default: hair id "1"
+  const [selectedFace, setSelectedFace] = useState(faces[0]);     // default: face id "1"
   const [selectedSkinColor, setSelectedSkinColor] = useState(null);
   const [selectedAccessory, setSelectedAccessory] = useState(null);
   const navigation = useNavigation();
@@ -44,79 +115,6 @@ const AvatarCreation = ({ }) => {
       }
     }
   };
-
-  const dark_hair = [
-    { id: "1", image: require("../../frontend/assets/dark_hair/hair-f1-dark-new.png") },
-    { id: "2", image: require("../../frontend/assets/dark_hair/hair-f2-dark-new.png") },
-    { id: "3", image: require("../../frontend/assets/dark_hair/hair-f3-dark-new.png") },
-    { id: "4", image: require("../../frontend/assets/dark_hair/hair-f4-dark-new.png") },
-    { id: "5", image: require("../../frontend/assets/dark_hair/hair-f5-dark-new.png") },
-    { id: "6", image: require("../../frontend/assets/dark_hair/hair-f6-dark-new.png") },
-    { id: "7", image: require("../../frontend/assets/dark_hair/hair-f7-dark-new.png") },
-    { id: "8", image: require("../../frontend/assets/dark_hair/hair-f8-dark-new.png") },
-    { id: "9", image: require("../../frontend/assets/dark_hair/hair-f9-dark-new.png") },
-    { id: "10", image: require("../../frontend/assets/dark_hair/hair-m1-dark-new.png") },
-    { id: "11", image: require("../../frontend/assets/dark_hair/hair-m2-dark-new.png") },
-    { id: "12", image: require("../../frontend/assets/dark_hair/hair-m3-dark-new.png") },
-    { id: "13", image: require("../../frontend/assets/dark_hair/hair-m4-dark-new.png") },
-    { id: "14", image: require("../../frontend/assets/dark_hair/hair-m5-dark-new.png") },
-  ];
-
-  const light_hair = [
-    { id: "1", image: require("../../frontend/assets/light_hair/hair-f1-light-new.png") },
-    { id: "2", image: require("../../frontend/assets/light_hair/hair-f2-light-new.png") },
-    { id: "3", image: require("../../frontend/assets/light_hair/hair-f3-light-new.png") },
-    { id: "4", image: require("../../frontend/assets/light_hair/hair-f4-light-new.png") },
-    { id: "5", image: require("../../frontend/assets/light_hair/hair-f5-light-new.png") },
-    { id: "6", image: require("../../frontend/assets/light_hair/hair-f6-light-new.png") },
-    { id: "7", image: require("../../frontend/assets/light_hair/hair-f7-light-new.png") },
-    { id: "8", image: require("../../frontend/assets/light_hair/hair-f8-light-new.png") },
-    { id: "9", image: require("../../frontend/assets/light_hair/hair-f9-light-new.png") },
-    { id: "10", image: require("../../frontend/assets/light_hair/hair-m1-light-new.png") },
-    { id: "11", image: require("../../frontend/assets/light_hair/hair-m2-light-new.png") },
-    { id: "12", image: require("../../frontend/assets/light_hair/hair-m3-light-new.png") },
-    { id: "13", image: require("../../frontend/assets/light_hair/hair-m4-light-new.png") },
-    { id: "14", image: require("../../frontend/assets/light_hair/hair-m5-light-new.png") },
-  ];
-
-  const faces = [
-    { id: "1", image: require("../../frontend/assets/faces/face-1.png") },
-    { id: "2", image: require("../../frontend/assets/faces/face-2.png") },
-    { id: "3", image: require("../../frontend/assets/faces/face-3.png") },
-    { id: "4", image: require("../../frontend/assets/faces/face-4.png") },
-    { id: "5", image: require("../../frontend/assets/faces/face-5.png") },
-    { id: "6", image: require("../../frontend/assets/faces/face-6.png") },
-    { id: "7", image: require("../../frontend/assets/faces/face-7.png") },
-    { id: "8", image: require("../../frontend/assets/faces/face-8.png") },
-    { id: "9", image: require("../../frontend/assets/faces/face-9.png") },
-    { id: "10", image: require("../../frontend/assets/faces/face-10.png") },
-    { id: "11", image: require("../../frontend/assets/faces/face-11.png") },
-    { id: "12", image: require("../../frontend/assets/faces/face-12.png") },
-    { id: "13", image: require("../../frontend/assets/faces/face-13.png") },
-    { id: "14", image: require("../../frontend/assets/faces/face-14.png") },
-    { id: "15", image: require("../../frontend/assets/faces/face-15.png") },
-  ];
-
-  const skin_color = [
-    { id: "1", image: require("../../frontend/assets/skin_colors/skin-0.png") },
-    { id: "2", image: require("../../frontend/assets/skin_colors/skin-1.png") },
-    { id: "3", image: require("../../frontend/assets/skin_colors/skin-2.png") },
-    { id: "4", image: require("../../frontend/assets/skin_colors/skin-3.png") },
-  ]
-
-  const accessories = [
-    { id: "1", image: require("../../frontend/assets/accessories/accessory-1.png") },
-    { id: "2", image: require("../../frontend/assets/accessories/accessory-2.png") },
-    { id: "3", image: require("../../frontend/assets/accessories/accessory-3.png") },
-    { id: "4", image: require("../../frontend/assets/accessories/accessory-4.png") },
-    { id: "5", image: require("../../frontend/assets/accessories/accessory-5.png") },
-    { id: "6", image: require("../../frontend/assets/accessories/accessory-6.png") },
-    { id: "7", image: require("../../frontend/assets/accessories/accessory-7.png") },
-    { id: "8", image: require("../../frontend/assets/accessories/accessory-8.png") },
-    { id: "9", image: require("../../frontend/assets/accessories/accessory-9.png") },
-  ]
-
-
 
   const displayedImages = (() => {
     if (whichScreen === 0) return isToggled ? light_hair : dark_hair;
@@ -170,11 +168,8 @@ const AvatarCreation = ({ }) => {
         {/* Avatar Preview */}
         <ViewShot ref={viewShotRef} options={{ format: "png", quality: 1.0 }}>
           <View className="w-72 h-72 my-6 bg-[#FFE7C0] rounded-full items-center justify-center overflow-hidden relative">
-            <Image
-              source={require("../../frontend/assets/head/base-head.png")}
-              className="h-96 w-96"
-              resizeMode="contain"
-            />
+
+
             {selectedHair && (
               <Image
                 source={selectedHair.image}
